cd9edf02220395c505236511ed6086716fa25177
tianxiawuzhei <1064639871@qq.cp>
2015-09-22 15:37:31 +0800

    标准socket的测试例子，摘自网络，包括服务端和客户端。

---
+++ serverSample/src/com/socket/server/mainEnter.java
@@ -0,0 +1,14 @@
+package com.socket.server;
+
+import com.socket.server.Server;
+
+public class mainEnter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       System.out.println("服务器启动...\n");    
+	       Server server = new Server();    
+	       server.init();  
+	}
+
+}
